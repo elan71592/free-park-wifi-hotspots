7e5b1bf85d9b42a51dc5023fe7c84090277ae414
Alicia Briceland & David Walden & Elan Kvitko <pair+abriceland.d-walden.elan71592@devbootcamp.com>
2016-04-29 15:14:39 -0400

    add color and ascii art

--- app/views/view.rb
+++ app/views/view.rb
@@ -1,5 +1,26 @@
+require 'colorize'
 class View
   def self.welcome
+    puts
+    puts
+    puts' /$$$$$$$$ /$$$$$$$  /$$$$$$$$ /$$$$$$$$       /$$      /$$ /$$$$$$ /$$$$$$$$ /$$$$$$'
+    puts'| $$_____/| $$__  $$| $$_____/| $$_____/      | $$  /$ | $$|_  $$_/| $$_____/|_  $$_/'
+    puts'| $$      | $$  \ $$| $$      | $$            | $$ /$$$| $$  | $$  | $$        | $$'
+    puts'| $$$$$   | $$$$$$$/| $$$$$   | $$$$$         | $$/$$ $$ $$  | $$  | $$$$$     | $$'
+    puts'| $$__/   | $$__  $$| $$__/   | $$__/         | $$$$_  $$$$  | $$  | $$__/     | $$'
+    puts'| $$      | $$  \ $$| $$      | $$            | $$$/ \  $$$  | $$  | $$        | $$'
+    puts'| $$      | $$  | $$| $$$$$$$$| $$$$$$$$      | $$/   \  $$ /$$$$$$| $$       /$$$$$$'
+    puts'|__/      |__/  |__/|________/|________/      |__/     \__/|______/|__/      |______/'
+    puts
+    puts' /$$   /$$  /$$$$$$  /$$$$$$$$          /$$$$$$  /$$$$$$$   /$$$$$$  /$$$$$$$$  /$$$$$$'
+    puts'| $$  | $$ /$$__  $$|__  $$__/         /$$__  $$| $$__  $$ /$$__  $$|__  $$__/ /$$__  $$'
+    puts'| $$  | $$| $$  \ $$   | $$           | $$  \__/| $$  \ $$| $$  \ $$   | $$   | $$  \__/'
+    puts'| $$$$$$$$| $$  | $$   | $$    /$$$$$$|  $$$$$$ | $$$$$$$/| $$  | $$   | $$   |  $$$$$$'
+    puts'| $$__  $$| $$  | $$   | $$   |______/ \____  $$| $$____/ | $$  | $$   | $$    \____  $$'
+    puts'| $$  | $$| $$  | $$   | $$            /$$  \ $$| $$      | $$  | $$   | $$    /$$  \ $$'
+    puts'| $$  | $$|  $$$$$$/   | $$           |  $$$$$$/| $$      |  $$$$$$/   | $$   |  $$$$$$/'
+    puts'|__/  |__/ \______/    |__/            \______/ |__/       \______/    |__/    \______/'
+    puts
     puts
     puts 'Welcome To The Free Park WiFi Hotspot Finder!'
     puts '============================================='
@@ -16,17 +37,17 @@ class View
   end
 
   def self.print_park_number( idx )
-    puts "Park #{idx + 1}"
+    puts "Park #{idx + 1}".underline
   end
 
   def self.print_park_info( park )
-    puts "Park Name: #{park.park}"
-    puts "Location Within Park: #{park.location}"
-    puts "Provider: #{park.provider}"
+    puts "Park Name: #{park.park}".green
+    puts "Location Within Park: #{park.location}".light_blue
+    puts "Provider: #{park.provider}".red
   end
 
   def self.print_note
-    puts "Note: Some WiFi are free but only offer access if you\nhave a current subscription for that provider!"
+    puts "Note: Some WiFi are free but only offer access if you\nhave a current subscription for that provider!".yellow
     puts "======================================================"
   end
 end
